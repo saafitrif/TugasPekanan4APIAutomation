4de038a4ba08ebe939234bbf19cd74efbdaa4d1f
Nur Safitri Febrianti <safitrifebrian93@gmail.com>
2023-06-25 23:23:37 +0700

    commit

--- createUser.js
+++ createUser.js
@@ -2,13 +2,13 @@ const request = require("supertest")("https://kasir-api.belajarqa.com");
 const expect = require("chai").expect;
 var token = "";
 
-
 describe("User - Create User", function()
 {
     //Test Case 3 (Success Create User)
     it ("TC_3_Success Create User ",async function(){
-        const response = await request.post("/users")
-        .set("Authorization", "Bearer " + token)
+        const response = await request
+        .post("/users")
+        .set("Authorization", "Bearer" + token)
         .send
             (
                 {
@@ -17,16 +17,18 @@ describe("User - Create User", function()
                     "password": "kasir1234@"
                  } 
             );
+
             expect(response.status).to.eql(201);
-            expect(response.body.name).to.equal(data.name);
-            expect(response.body.email).to.equal(data.email);
+            expect(response.body.data.name).to.equal("Kasir 1");
+            expect(response.body.data.email).to.equal("kasir1@ex.com");
 
         });
     
     //Test Case 4 (Failed Create User)
     it ("TC_4_Failed Create User",async function(){
-        const response = await request.post("/users")
-        .set("Authorization", "Bearer " + token)
+        const response = await request
+        .post("/users")
+        .set("Authorization", "Bearer")
         .send
             (
                 {
@@ -36,7 +38,7 @@ describe("User - Create User", function()
                  }
                  
             );
-            expect(response.status).to.eql(400);
+            expect(response.status).to.eql(401);
 
         });
 }
